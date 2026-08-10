5c861ab43c76582616e8a254e40eddda20f02523
Pasquale Palombo <pasqualepalombo@outlook.com>
2026-08-10 20:25:20 +0200

    update 100826 first API

---
+++ GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,11 @@
+namespace GameStore.Api.Dtos;
+
+//Il Data Transfer Object è il formato di comune accordo con cui client e server
+//trasferiscono l'oggetto e le sue caratteristiche
+public record class GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,23 @@
 //Sezione di application configuration
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+//LISTA GIOCHI DA TOGLIERE POI
+List<GameDto> games = [
+    new (1, "The Blue Nowhere", "Adventure", 19.99m, new DateOnly(2027, 3, 5)),
+    new(2, "Mi.mi.co", "Dungeon Crawler", 19.95m, new DateOnly(2028, 8, 11)),
+    new(3, "Wildfire", "Adventure", 35.00m, new DateOnly(2028, 12, 24))
+];
+
+
 //Sezione HTTP Request Pipeline
-app.MapGet("/", () => "Hello World!");
+// GET endpoints
+app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", () => "Gioco preciso");
+
 
 
 app.Run();
